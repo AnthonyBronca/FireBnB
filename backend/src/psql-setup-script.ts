a268ee38f5179b32f8b4213743a94dc2331b35c9
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2023-07-30 02:40:21 -0400

    removed dist folder

--- backend/src/psql-setup-script.ts
+++ backend/src/psql-setup-script.ts
@@ -1,6 +1,6 @@
 const { sequelize } = require('./db/models');
 
-sequelize.showAllSchemas({ logging: false }).then(async (data) => {
+sequelize.showAllSchemas({ logging: false }).then(async (data:any) => {
     if (!data.includes(process.env.SCHEMA)) {
         await sequelize.createSchema(process.env.SCHEMA);
     }
