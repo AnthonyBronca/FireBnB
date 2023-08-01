e099fff2d42d56743b362e2088b0d0bab383888f
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2023-08-01 14:59:44 -0400

    added typescript migration test and typescript seeder test

---
+++ backend/src/db/seeders/01-test-users-seeders.ts
@@ -0,0 +1,41 @@
+'use strict';
+
+import { OptionsInterface } from "../../typings/seeders";
+
+const bcrypt = require("bcryptjs");
+
+let options:OptionsInterface = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  up: async (queryInterface:any, Sequelize:any) => {
+    options.tableName = 'TestUsers';
+    return queryInterface.bulkInsert(options, [
+      {
+        email: 'demo123@user.io',
+        username: 'Demo-lition123123',
+        hashedPassword: bcrypt.hashSync('password')
+      },
+      {
+        email: 'user123@user.io',
+        username: 'FakeUser123',
+        hashedPassword: bcrypt.hashSync('password2')
+      },
+      {
+        email: 'user232@user.io',
+        username: 'FakeUser212',
+        hashedPassword: bcrypt.hashSync('password3')
+      }
+    ], {});
+  },
+
+  down: async (queryInterface:any, Sequelize:any) => {
+    options.tableName = 'TestUsers';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
+    }, {});
+  }
+};
