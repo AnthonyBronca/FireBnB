59ee1070806b1f6576cf609197ec34bcf8c863f4
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2023-09-13 16:29:57 -0400

    added some format and created a reviews file

---
+++ backend/src/db/migrations/05-reviews.ts
@@ -0,0 +1,63 @@
+"use strict";
+
+import { OptionsInterface } from "../../typings/seeders";
+
+let options:OptionsInterface = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  up: async (queryInterface:any, Sequelize:any) => {
+    return queryInterface.createTable("Reviews", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      address: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        unique: true,
+      },
+      city: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      state: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      description: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      lat: {
+        type: Sequelize.INTEGER,
+      },
+      long: {
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: { model: "Users", schema: 'schema'}
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    }, options);
+  },
+  down: async (queryInterface:any, Sequelize:any) => {
+    options.tableName = "Reviews";
+    return queryInterface.dropTable(options);
+  }
+};
