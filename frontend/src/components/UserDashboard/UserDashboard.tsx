e428453dc1367c40164616cd5b3263157c7c14c8
OptimumMars <gabriel.gutz@live.com>
2023-11-09 17:13:03 -0500

    added a userdashboard with general layout

--- frontend/src/components/UserDashboard/UserDashboard.tsx
+++ frontend/src/components/UserDashboard/UserDashboard.tsx
@@ -1,11 +1,61 @@
 import { useAppSelector } from '../../store';
+import './UserDashboard.css'
 
 const UserDashboard = () => {
     const user = useAppSelector((state) => state.session.user);
 
-    console.log(user);
+    const handleFutureFeature = () => {
+        alert("This feature is in development and will be released soon!");
+    }
+
     return (
-        <div>hehe haha</div>
+        <div>
+            <h1>
+                Account
+            </h1>
+            {user && <div>
+                {`${user.firstName} ${user.lastName}, ${user.email} * `}
+                <span onClick={handleFutureFeature}>Go to Profile</span>
+            </div>}
+
+            <div className="card-container">
+                <div onClick={handleFutureFeature} className='account-card'>
+                    <span>Personal Info</span>
+                </div>
+                <div onClick={handleFutureFeature} className='account-card'>
+                    <span>Login & security</span>
+                </div>
+                <div onClick={handleFutureFeature} className='account-card'>
+                    <span>Payments & payouts</span>
+                </div>
+                <div onClick={handleFutureFeature} className='account-card'>
+                    <span>Taxes</span>
+                </div>
+                <div onClick={handleFutureFeature} className='account-card'>
+                    <span>Notifications</span>
+                </div>
+                <div onClick={handleFutureFeature} className='account-card'>
+                    <span>Privacy & sharing</span>
+                </div>
+                <div onClick={handleFutureFeature} className='account-card'>
+                    <span>Global preferences</span>
+                </div>
+                <div onClick={handleFutureFeature} className='account-card'>
+                    <span>Travel for work</span>
+                </div>
+                <div onClick={handleFutureFeature} className='account-card'>
+                    <span>Professional hosting tools</span>
+                </div>
+                <div onClick={handleFutureFeature} className='account-card'>
+                    <span>Referral credit & coupon</span>
+                </div>
+            </div>
+
+            <div>
+                <p>Need to deactivate your account?</p>
+                <span onClick={handleFutureFeature}>Take care of that now</span>
+            </div>
+        </div>
     )
 }
 
