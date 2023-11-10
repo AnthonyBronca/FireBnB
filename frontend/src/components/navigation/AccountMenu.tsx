f9aa4e920ba5c8b5747a6b833bfd3250a9867519
OptimumMars <gabriel.gutz@live.com>
2023-11-09 23:07:15 -0500

    small merge issue fixed

--- frontend/src/components/navigation/AccountMenu.tsx
+++ frontend/src/components/navigation/AccountMenu.tsx
@@ -28,89 +28,88 @@ const AccountMenu = () => {
         }
     }
 
-    const handleModalOpen = (e: any) => {
-        const handleModalOpen = (e: any, buttonClicked: string) => {
-            e.preventDefault();
-            toggleOpen(buttonClicked);
-            handleSignIn();
-        }
-
-
-        const handleSignIn = () => {
-            // e.preventDefault();
-            // e.stopPropagation();
-            if (isOpen) {
-                setIsOpen(false)
-            } else {
-                setIsOpen(true)
-            }
-            // dispatch(login({credential: 'anthony@user.io', password: 'password3'}))
-        }
-
-        const handleLogout = (e: MouseEvent) => {
-            e.preventDefault();
-            e.stopPropagation();
-            setMenuOpen(false);
-            dispatch(logout()).then(() => window.location.reload())
-        }
-
-        const navigate = useNavigate();
-
-        const handleAccount = (e: MouseEvent) => {
-            e.preventDefault();
-            e.stopPropagation();
-            navigate("/account");
-        }
-
-        const handleFutureFeature = () => {
-            alert("This feature is in development and will be released soon!");
-            handleMenuOpen();
-        }
-
-        return (
-            <div
-                onClick={handleMenuOpen}
-                className='nav-bar-drop-down-container'>
-                <div className='user-container'>
-                    <div className='nav-bar-drop-down-buttons'>
-                        <img src={hamburger} alt='hamburger menu' className='hamburger-icon' />
-                        {user ?
-                            <div className="user-letter-container">
-                                <span className='user-letter'>A</span>
-                                <div className='notification-container'>
-                                    <span className='notification-num'>1</span>
-                                </div>
-                            </div>
-                            : <img src={usericon} className='anon-avatar-icon' alt='avatar logo' />}
-                    </div>
-                </div>
-                {user ? <>
-                    {menuOpen ? <div className='drop-down-container'>
-                        <span onClick={handleFutureFeature} className='reg-span'>Messages</span>
-                        <span onClick={handleFutureFeature} className='reg-span'>Notifications</span>
-                        <span onClick={handleFutureFeature} className='reg-span'>Trips</span>
-                        <span onClick={handleFutureFeature} className='reg-span'>Wishlists</span>
-                        <div onClick={handleFutureFeature} className='horizontal-line'></div>
-                        <span onClick={handleFutureFeature} className='unreg-span'>Firebnb your home</span>
-                        <span onClick={handleAccount} className='unreg-span'>Account</span>
-                        <div onClick={handleFutureFeature} className='horizontal-line'></div>
-                        <span onClick={handleFutureFeature} className='unreg-span'>Help Center</span>
-                        <span onClick={(e: any) => handleLogout(e)} className='unreg-span'>Log out</span>
-                    </div>
-                        : null}
-                </>
-                    : <>
-                        {menuOpen ? <div className='drop-down-container-not-logged'>
-                            <span onClick={(e: any) => handleModalOpen(e)} className='reg-span'>Sign up</span>
-                            <span onClick={(e: any) => handleModalOpen(e)} className='unreg-span'>Log in</span>
-                            <Divider />
-                            <span onClick={handleFutureFeature} className='unreg-span'>Firebnb your home</span>
-                            <span onClick={handleFutureFeature} className='unreg-span'>Help Center</span>
-                        </div> : null}
-                    </>
-                }
-            </div>
-        );
+    const handleModalOpen = (e: any, buttonClicked: string) => {
+        e.preventDefault();
+        toggleOpen(buttonClicked);
+        handleSignIn();
     }
 
-    export default AccountMenu;
+
+    const handleSignIn = () => {
+        // e.preventDefault();
+        // e.stopPropagation();
+        if (isOpen) {
+            setIsOpen(false)
+        } else {
+            setIsOpen(true)
+        }
+        // dispatch(login({credential: 'anthony@user.io', password: 'password3'}))
+    }
+
+    const handleLogout = (e: MouseEvent) => {
+        e.preventDefault();
+        e.stopPropagation();
+        setMenuOpen(false);
+        dispatch(logout()).then(() => window.location.reload())
+    }
+
+    const navigate = useNavigate();
+
+    const handleAccount = (e: MouseEvent) => {
+        e.preventDefault();
+        e.stopPropagation();
+        navigate("/account");
+    }
+
+    const handleFutureFeature = () => {
+        alert("This feature is in development and will be released soon!");
+        handleMenuOpen();
+    }
+
+    return (
+        <div
+            onClick={handleMenuOpen}
+            className='nav-bar-drop-down-container'>
+            <div className='user-container'>
+                <div className='nav-bar-drop-down-buttons'>
+                    <img src={hamburger} alt='hamburger menu' className='hamburger-icon' />
+                    {user ?
+                        <div className="user-letter-container">
+                            <span className='user-letter'>A</span>
+                            <div className='notification-container'>
+                                <span className='notification-num'>1</span>
+                            </div>
+                        </div>
+                        : <img src={usericon} className='anon-avatar-icon' alt='avatar logo' />}
+                </div>
+            </div>
+            {user ? <>
+                {menuOpen ? <div className='drop-down-container'>
+                    <span onClick={handleFutureFeature} className='reg-span'>Messages</span>
+                    <span onClick={handleFutureFeature} className='reg-span'>Notifications</span>
+                    <span onClick={handleFutureFeature} className='reg-span'>Trips</span>
+                    <span onClick={handleFutureFeature} className='reg-span'>Wishlists</span>
+                    <div onClick={handleFutureFeature} className='horizontal-line'></div>
+                    <span onClick={handleFutureFeature} className='unreg-span'>Firebnb your home</span>
+                    <span onClick={handleAccount} className='unreg-span'>Account</span>
+                    <div onClick={handleFutureFeature} className='horizontal-line'></div>
+                    <span onClick={handleFutureFeature} className='unreg-span'>Help Center</span>
+                    <span onClick={(e: any) => handleLogout(e)} className='unreg-span'>Log out</span>
+                </div>
+                    : null}
+            </>
+                : <>
+                    {menuOpen ? <div className='drop-down-container-not-logged'>
+                        <span onClick={(e: any) => handleModalOpen(e)} className='reg-span'>Sign up</span>
+                        <span onClick={(e: any) => handleModalOpen(e)} className='unreg-span'>Log in</span>
+                        <Divider />
+                        <span onClick={handleFutureFeature} className='unreg-span'>Firebnb your home</span>
+                        <span onClick={handleFutureFeature} className='unreg-span'>Help Center</span>
+                    </div> : null}
+                </>
+            }
+        </div>
+    );
+}
+
+export default AccountMenu;
