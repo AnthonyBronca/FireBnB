3b391a84df609c014ab96846ac4fa4ec6196af01
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2023-12-21 01:26:14 -0500

    added option menu

--- frontend/src/components/BookingForm/BookingForm.tsx
+++ frontend/src/components/BookingForm/BookingForm.tsx
@@ -1,23 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './BookingForm.css'
-
+import { guestCount } from './guestCount';
 
 const BookingForm: React.FC = (): JSX.Element => {
+    const [startDate, setStartDate] = useState<string>('')
+    const [endDate, setEndDate] = useState<string>('');
+    const [guest, setGuest] = useState<string>('');
+    console.log(guest)
+
+    const setStart = (e: React.FocusEvent<HTMLInputElement, Element>) => {
+        setStartDate(e.target.value);
+    };
+
+    const setEnd = (e: React.FocusEvent<HTMLInputElement, Element>) => {
+        setEndDate(e.target.value);
+    }
+
+    const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setGuest(e.target.value);
+    }
+
   return (
     <div className='booking-form-container'>
         <div className='price-container'>
             <span>$175</span>
             <span>night</span>
         </div>
-        <div className='date-container'>
-            <div className='date-container-top'>
-                <input placeholder='1/21/2024'/>
-                <input placeholder='1/22/2024'/>
+        <form>
+            <div className='date-container'>
+                <div className='date-container-top'>
+                    <input type='date' className='date-left-input' onFocus={(e) => setStart(e)}/>
+                    <input type='date' className='date-right-input' onFocus={(e) => setEnd(e)}/>
+                </div>
+                <div className='date-container-bottom'>
+                    <select
+                        value={guest}
+                        onChange={handleSelectChange}
+                    >
+                    <option value='' hidden disabled>Guests</option>
+                        {guestCount.map((num)=> (
+                            <option key={num} value={num}>{num}</option>
+                        ))}
+                    </select>
+                </div>
             </div>
-            <div className='date-container-bottom'>
-                <input placeholder='guests'/>
-            </div>
-        </div>
+        </form>
     </div>
   );
 }
