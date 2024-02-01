faae96237b42cc02c5a4e6ceeca5534473b191bc
Krystal <kabkimmel@gmail.com>
2024-01-31 19:19:05 -0600

    created empty content tabs for explore and wishlist features

--- mobile/screens/Home/Components/NavBar.tsx
+++ mobile/screens/Home/Components/NavBar.tsx
@@ -1,15 +1,54 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { IconDefinition, faHeart } from '@fortawesome/free-regular-svg-icons';
 import Test from './Test';
 
-const Tab = createBottomTabNavigator();
 
-const NavBar = () => {
+const Tab = createBottomTabNavigator();
+const home = "Home";
+const explore = "Explore";
+const wishlists = "Wishlists";
+const trips = "Trips";
+const inbox = "Inbox";
+const login = "Log In";
+let iconName:IconDefinition;
+
+
+const NavBar:React.FC = ():JSX.Element => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Test" component={Test} />
-    </Tab.Navigator>
+      <Tab.Navigator initialRouteName={home} screenOptions={({route}) => ({
+        tabBarIcon: ({ focused, color, size}) => {
+          let rn = route.name;
+      
+          if (rn === explore && focused) {
+            iconName = faMagnifyingGlass
+            color = "#ff375d"
+          } 
+          if (rn === wishlists && focused) {
+            iconName = faHeart
+            color = "#ff375d"
+          }
+          if (rn === trips && focused) {
+            iconName = faHeart
+            color = "#ff375d"
+          }
+          if (rn === inbox && focused) {
+            iconName = faHeart
+            color = "#ff375d"
+          }
+          if (rn === login && focused) {
+            iconName = faHeart
+            color = "#ff375d"
+          }
+          return <FontAwesomeIcon icon={iconName} color={color} size={size} />
+        }
+      })}>
+        <Tab.Screen name={explore} component={Test} />
+        <Tab.Screen name={wishlists} component={Test} />
+      </Tab.Navigator>
   );
 }
 
