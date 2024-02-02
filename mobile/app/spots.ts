a3df816771a32751bbdc371436fdbba25c20f2b7
Krystal <kabkimmel@gmail.com>
2024-02-02 16:18:04 -0600

    Completed reducer for spots

--- mobile/app/spots.ts
+++ mobile/app/spots.ts
@@ -43,14 +43,80 @@ export const SpotSlice = createSlice({
                 state.allSpots.push(action.payload);
             };
         },
-        // setSpot,
-        // getUserSpots,
-        // editSpot,
-        // deleteSpot
-
+        setSpot: (state, action: PayloadAction<Spot>) => {
+            if(state.byId){
+                state.byId[`${action.payload.id}`] = action.payload;
+            };
+        },
+        getUserSpots: (state, action: PayloadAction<{Spots: Spot[]}>) => {
+            state.userSpotId = {};
+            state.userSpots = action.payload.Spots;
+            for (let spot of action.payload.Spots){
+                if(!state.userSpotId[`${spot.id}`]){
+                    state.userSpotId[`${spot.id}`] = spot;
+                };
+            };
+        },
+        editSpot: (state, action: PayloadAction<Spot>) => {
+            if(state.userSpotId && state.userSpotId[`${action.payload.id}`]){
+                state.userSpotId[`${action.payload.id}`].name = action.payload.name;
+                state.userSpotId[`${action.payload.id}`].price = action.payload.price;
+            };
+            if(state.byId && state.byId[`${action.payload.id}`]){
+                state.byId[`${action.payload.id}`].name = action.payload.name;
+                state.byId[`${action.payload.id}`].price = action.payload.price;
+            };
+            if(state.userSpots && state.userSpots.length > 0){
+                for(let i = 0; i < state.userSpots.length; i++){
+                 let spot = state.userSpots[i];
+                 if(spot.id === action.payload.id){
+                     state.userSpots[i].name = action.payload.name;
+                     state.userSpots[i].price = action.payload.price;
+                     break;
+                 }
+                }
+            };
+            if(state.allSpots && state.allSpots.length > 0){
+                for(let i = 0; i < state.allSpots.length; i++){
+                 let spot = state.allSpots[i];
+                 if(spot.id === action.payload.id){
+                     state.allSpots[i].name = action.payload.name;
+                     state.allSpots[i].price = action.payload.price;
+                     break;
+                 }
+                }
+            };
+        },
+        deleteSpot: (state, action: PayloadAction<Spot>) => {
+            if(state.allSpots && state.allSpots.length > 0){
+                let newState = state.allSpots.filter(spot => {
+                    return spot.id !== action.payload.id;
+                })
+                state.allSpots = newState;
+            };
+            if(state.byId && state.byId[`${action.payload.id}`]){
+                delete state.byId[`${action.payload.id}`]
+            };
+            if(state.userSpots && state.userSpots.length > 0){
+                let newState = state.userSpots.filter(spot => {
+                    return spot.id !== action.payload.id;
+                })
+                state.userSpots = newState;
+            };
+            if(state.userSpotId && state.userSpotId[`${action.payload.id}`]){
+                delete state.userSpotId[`${action.payload.id}`];
+            };
+        },
     },
 });
 
-export const { } = SpotSlice.actions;
+export const { 
+    setSpots, 
+    addSpot, 
+    setSpot, 
+    getUserSpots, 
+    editSpot, 
+    deleteSpot 
+} = SpotSlice.actions;
 
 export default SpotSlice.reducer;
