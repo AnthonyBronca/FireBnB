502bedf4cf7687fb6bdd69aa88407f0aeb4908ed
Krystal <kabkimmel@gmail.com>
2024-02-05 14:39:26 -0600

    commented out code for now

--- mobile/awsS3.ts
+++ mobile/awsS3.ts
@@ -1,7 +1,7 @@
-import {
-    S3Client,
-    PutObjectCommand,
-  } from "@aws-sdk/client-s3";
+// import {
+//     S3Client,
+//     PutObjectCommand,
+//   } from "@aws-sdk/client-s3";
 //   return `https://${params.Bucket}.s3${regionString}.amazonaws.com/${fileName}`
 
 
@@ -9,48 +9,47 @@ import {
 
 // grab the URI of the uploaded image
 
-const filesToUpload = [
-    {
-      Key: "demofile3.txt",
-      Body: "Content of file 3",
-      ACL: "public-read",
-    },
-    {
-      Key: "demofile4.txt",
-      Body: "Content of file 4",
-      ACL: "public-read",
-    },
-    // Add more files as needed
-  ];
+// const filesToUpload = [
+//     {
+//       Key: "demofile3.txt",
+//       Body: "Content of file 3",
+//       ACL: "public-read",
+//     },
+//     {
+//       Key: "demofile4.txt",
+//       Body: "Content of file 4",
+//       ACL: "public-read",
+//     },
+//   ];
 
-interface IFileObject {
-    Key: string;
-    Body: string;
-    ACL: string | any;
-  }
+// interface IFileObject {
+//     Key: string;
+//     Body: string;
+//     ACL: string | any;
+//   }
   
-  export async function uploadFilesToS3(files: IFileObject[]) {
-    const s3Client = new S3Client({ region: '' });
-    const S3_BUCKET = '';
+//   export async function uploadFilesToS3(files: IFileObject[]) {
+//     const s3Client = new S3Client({ region: '' });
+//     const S3_BUCKET = '';
   
-    try {
-      for (const file of files) {
-        await s3Client.send(new PutObjectCommand({
-          Bucket: S3_BUCKET,
-          Key: file.Key,
-          Body: file.Body,
-          ACL: file.ACL,
-        }));
-        console.log(`Upload successful for file: ${file.Key}`);
-      }
-      console.log("All files uploaded successfully!");
-    } catch (error) {
-      console.error("There was an error uploading to S3:", error);
-      throw error; 
-    }
-  };
+//     try {
+//       for (const file of files) {
+//         await s3Client.send(new PutObjectCommand({
+//           Bucket: S3_BUCKET,
+//           Key: file.Key,
+//           Body: file.Body,
+//           ACL: file.ACL,
+//         }));
+//         console.log(`Upload successful for file: ${file.Key}`);
+//       }
+//       console.log("All files uploaded successfully!");
+//     } catch (error) {
+//       console.error("There was an error uploading to S3:", error);
+//       throw error; 
+//     }
+//   };
 
-  uploadFilesToS3(filesToUpload);
+//   uploadFilesToS3(filesToUpload);
 
 
 
