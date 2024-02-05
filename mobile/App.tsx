d0035d8f5754c6ba5066f6d05d7f5f1f578886cb
Krystal <kabkimmel@gmail.com>
2024-02-05 14:00:37 -0600

    wrapped RootNavigation component with redux provider

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,11 @@
 import RootNavigation from './Navigation';
+import { store } from './app/store';
+import { Provider as ReduxProvider } from 'react-redux';
 
 export default function App() {
   return (
-    <RootNavigation/>
+    <ReduxProvider store={store}>
+      <RootNavigation/>
+    </ReduxProvider>
   );
 };
