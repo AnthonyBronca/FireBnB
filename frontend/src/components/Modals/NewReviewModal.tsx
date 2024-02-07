a22340c2c9c4b8deb00b00c53102fcfe51f5162a
Alex Bettinger <alexi56@comcast.net>
2024-02-07 12:03:32 -0700

    skeleton and styling

--- frontend/src/components/Modals/NewReviewModal.tsx
+++ frontend/src/components/Modals/NewReviewModal.tsx
@@ -1,8 +1,9 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import './css/NewReviewModalStyles.css'
 //import { useAppSelector } from '../../store';
 
 import NewReviewModalContext from '../../context/NewReviewModalContext';
+import { TextareaAutosize } from '@mui/material';
 
 interface INewReviewProps {
     spotId: number
@@ -10,15 +11,29 @@ interface INewReviewProps {
 
 const NewReviewModal: React.FC<INewReviewProps> = ({ spotId }):JSX.Element => {
     const {togglePostReviewOpen } = useContext(NewReviewModalContext);
-    const handleOpen = () => {
+
+    const handleClose = () => {
         togglePostReviewOpen(true)
     }
+
+    useEffect(()=> {
+        const loginmodal = document.getElementById('new-review-modal');
+        if(loginmodal){
+            loginmodal.addEventListener('click', (e)=> {
+                if(e.target !== null){
+                    if(e.target === e.currentTarget){
+                        handleClose();
+                    }
+                }
+            })
+        }
+    })
 
     return (
         <section id="new-review-modal">
             <div id="nr-modal-content">
                 <div id="exit-parent">
-                    <span onClick={handleOpen} id="nr-exit-btn">
+                    <span onClick={handleClose} id="nr-exit-btn">
                         X
                     </span>
                 </div>
@@ -26,9 +41,17 @@ const NewReviewModal: React.FC<INewReviewProps> = ({ spotId }):JSX.Element => {
                     <h1>Write a public review</h1>
                     <p>Tell the next guests what you loved, and anything else about this place.</p>
                 </div>
-                <div>Input box</div>
-                <div>stars</div>
-                <div>submit btn</div>
+                <form id="review-form">
+                    <TextareaAutosize
+                        id="rvw-txt-area"
+                        minRows={4}
+                        placeholder='How was your stay?'
+                    />
+                    <div id="review-stars">
+                        1 2 3 4 5
+                    </div>
+                    <button type="submit">Submit Review</button>
+                </form>
             </div>
         </section>
     )
