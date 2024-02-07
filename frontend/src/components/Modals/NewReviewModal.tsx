3ab5fcbb833a647dee6265feb8c89837a1a982cd
Alex Bettinger <alexi56@comcast.net>
2024-02-06 22:50:58 -0700

    more styling

--- frontend/src/components/Modals/NewReviewModal.tsx
+++ frontend/src/components/Modals/NewReviewModal.tsx
@@ -17,14 +17,18 @@ const NewReviewModal: React.FC<INewReviewProps> = ({ spotId }):JSX.Element => {
     return (
         <section id="new-review-modal">
             <div id="nr-modal-content">
-                <span onClick={handleOpen}>
-                    X
-                </span>
-                <div>
-                    <p>Write a public review</p>
+                <div id="exit-parent">
+                    <span onClick={handleOpen} id="nr-exit-btn">
+                        X
+                    </span>
+                </div>
+                <div id="nrm-header">
+                    <h1>Write a public review</h1>
                     <p>Tell the next guests what you loved, and anything else about this place.</p>
                 </div>
-                <p>{spotId}</p>
+                <div>Input box</div>
+                <div>stars</div>
+                <div>submit btn</div>
             </div>
         </section>
     )
