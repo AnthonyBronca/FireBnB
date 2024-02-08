787f02ac5b89b6d4c579225fbc11c91752dca54f
Krystal <kabkimmel@gmail.com>
2024-02-07 21:17:12 -0600

    removed imports and code that were no longer necessary

--- mobile/screens/Home/Home.tsx
+++ mobile/screens/Home/Home.tsx
@@ -1,46 +1,15 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet} from 'react-native';
-
-// import { useAppDispatch, useAppSelector } from '../../store';
-// import { fetchSpots } from '../../store/spots';
 import BottomTabs from './Components/NavBar';
 import Search from './Components/Search';
 import Spots from './Components/Spots';
-
 
 interface IHome {
   navigation: any
 }
 
+
 const Home: React.FC<IHome> = ({ navigation }) => {
-
-
-
-/*
-[
-"address": "71720 Laron Plains",
-"avgRating": "NEW",
-"city": "Dublin",
-"country": "United States",
-"createdAt": "2024-1-6",
-"description": "Great place to stay when you want to get away from it all",
-"id": 20,
-"lat": "-41.946337",
-"lng": "-29.062167",
-"name": "Cuban-style home",
-"ownerId": 3, "previewImage":
-"https://harbr.de/fileadmin/_processed_/a/1/csm_harbr_boardinghouse_ludwigsburg_apartment_comfort_02_8fdc0763bd.jpg",
-"price": 62,
-"reviews": [],
-"state":
-"Wisconsin",
-"updatedAt":
-"2024-1-6",
-"userId": 3}
-]
-
-*/
-
   return (
     <SafeAreaView style={styles.container}>
       <Search/>
@@ -53,7 +22,6 @@ const Home: React.FC<IHome> = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // backgroundColor: 'blue'
   }
 })
 
