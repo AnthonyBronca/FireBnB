30a14de048ec34082272aa6e9a65ceac1cecaea3
Krystal <kabkimmel@gmail.com>
2024-02-07 22:38:55 -0600

    Added heart icon and "guest favorite" text to every spot with an even-numbered id

--- mobile/screens/Home/Components/Spots.tsx
+++ mobile/screens/Home/Components/Spots.tsx
@@ -1,25 +1,28 @@
 import React, {useEffect, useState} from 'react';
 import { useAppDispatch, useAppSelector } from '../../../store';
-import { View, ScrollView, Image, Pressable, StyleSheet } from 'react-native';
+import { View, ScrollView, Image, Text, Pressable, StyleSheet } from 'react-native';
 import { fetchSpots } from '../../../store/spots';
 import SpotDetails from '../../Home/Components/SpotDetails'
-
-
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { fonts } from '../../../constants/stylings/styles';
 
 interface IHome {
     navigation: any
 };
 
-
+interface IHeartPressed {
+    [key:number]: boolean
+}
 
 
 const Spots:React.FC<IHome> = ({navigation}) => {
     const dispatch = useAppDispatch();
     const allSpots = useAppSelector((state) => state.spots.allSpots);
-    const [isLoaded, setIsLoaded] = useState<boolean>(false);
     const date = new Date().getDate();
     const sec = new Date().getSeconds();
-
+    const [isLoaded, setIsLoaded] = useState<boolean>(false);
+    const [isHeartPressed, setIsHeartPressed] = useState<IHeartPressed>({});
 
     useEffect(() => {
         const getInfo = async() => {
@@ -29,16 +32,39 @@ const Spots:React.FC<IHome> = ({navigation}) => {
         getInfo()
     }, [isLoaded]);
 
+    const toggleHeartPress = (spotId:number) => {
+        setIsHeartPressed(prev => ({
+            ...prev,
+            [spotId]: !prev[spotId]
+        }));
+    };
+
     const goToSpotDetail = () => {
         navigation.push('SpotDetail')
     };
 
     return (
     <ScrollView>
-        {allSpots?.map((spot) => (
+        {allSpots?.map((spot, idx) => (
             <View 
-                key={`spot-${spot.id}-${date}-${sec}`}
-                style={styles.spotImageView}>
+                key={`spot-${idx}-${date}-${sec}`}
+                style={styles.spotImageView}
+            >
+            <Pressable 
+                style={styles.heartIcon}
+                onPress={() => toggleHeartPress(spot.id)}
+            >   
+                {idx % 2 === 0 && (
+                    <View style={styles.guestFavContainer}>
+                        <Text style={styles.guestFavText}>Guest favorite</Text>
+                    </View>
+                )}
+                <FontAwesomeIcon 
+                    icon={faHeart} 
+                    size={25} 
+                    color={isHeartPressed[spot.id] ? '#FF385B' :'#535350'}
+                />
+            </Pressable>
             <Pressable
                 style={styles.spotImageContainer} 
                 onPress={goToSpotDetail}
@@ -74,6 +100,26 @@ image: {
     objectFit: 'fill',
     borderRadius: 10,
     backgroundColor:'#FFFFFF',
+},
+heartIcon: {
+    position: 'absolute',
+    top: 10,
+    right: 45,
+    zIndex: 1
+},
+guestFavContainer: {
+    backgroundColor: "#FBFBFB",
+    borderColor: '#A19F9D',
+    borderRadius: 20,
+    position: 'absolute',
+    right: 220,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 125,
+    height: 25,
+},
+guestFavText: {
+    ...fonts.subHeader
 }
 });
 
