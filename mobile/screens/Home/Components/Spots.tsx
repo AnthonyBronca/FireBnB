9a18629a8695290576bb0ee46e2cee08753defc6
Krystal <kabkimmel@gmail.com>
2024-02-07 19:12:34 -0600

    Modified styling for a spot image

--- mobile/screens/Home/Components/Spots.tsx
+++ mobile/screens/Home/Components/Spots.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import { useAppDispatch, useAppSelector } from '../../../store';
-import { View, Image, TouchableOpacity } from 'react-native';
+import { View, Image, TouchableOpacity, StyleSheet } from 'react-native';
 import { fetchSpots } from '../../../store/spots';
 
 
@@ -27,17 +27,44 @@ const Spots:React.FC<IHome> = ({navigation}) => {
         navigation.push('SpotDetail')
       }
 
+      console.log('data--', data)
+
       return (
         <View>
-            <TouchableOpacity onPress={goToSpotDetail}>
-                <Image 
-                style={{ width: 400, height: 400, objectFit: 'contain' }} 
-                source={{ uri: "https://harbr.de/fileadmin/_processed_/a/1/csm_harbr_boardinghouse_ludwigsburg_apartment_comfort_02_8fdc0763bd.jpg"}}
-                />
-            </TouchableOpacity>
- 
+            <View style={styles.spotImageView}>
+                <TouchableOpacity
+                    style={styles.spotImageContainer} 
+                    onPress={goToSpotDetail}
+                   
+                >
+                    <Image 
+                    style={styles.image} 
+                    source={{ uri: "https://harbr.de/fileadmin/_processed_/a/1/csm_harbr_boardinghouse_ludwigsburg_apartment_comfort_02_8fdc0763bd.jpg"}}
+                    />
+                </TouchableOpacity>
+            </View>
         </View>
       );
-    }
+    };
+
+    const styles = StyleSheet.create({
+        spotImageView: {
+            alignItems: 'center',
+            marginVertical: 20
+        },
+        spotImageContainer: {
+            backgroundColor:'green',
+            height: 265,
+            width: 400,
+            alignItems: 'center',
+            justifyContent: 'center'
+        },
+        image: {
+            width: 400,
+            height: 265,
+            objectFit: 'contain',
+            backgroundColor:'blue',
+        }
+    })
 
 export default Spots;
