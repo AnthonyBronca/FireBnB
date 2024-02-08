1df8aaab01361a9955090837b164e8b6f944cf7f
Krystal <kabkimmel@gmail.com>
2024-02-07 18:34:34 -0600

    styled search bar and temporary filer icon

--- mobile/screens/Home/Components/Search.tsx
+++ mobile/screens/Home/Components/Search.tsx
@@ -1,55 +1,33 @@
-import React, { useState } from 'react';
-import { SafeAreaView, View, Text, StyleSheet } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faSearch, faFilter } from '@fortawesome/free-solid-svg-icons';
 
 
 const Search:React.FC = () => {
-    // const [searchInput, setSearchInput] = useState("")
-
-    // const handleInputChange = (input: string) => {
-    //     setSearchInput(input);
-    // };
 
   return (
-    <SafeAreaView >
         <View style={styles.searchBarComponentContainer}>
-            <TouchableOpacity style={styles.searchBar}>
-                <FontAwesomeIcon icon={faSearch} style={styles.searchIcon} size={20}/>
-                <Text style={styles.placeholderHeading}>Where To?</Text>
-                <Text style={styles.placeholderSubheading}>Anywhere · Any week · Add guests</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.filterIconContainer}>
-                <FontAwesomeIcon icon={faFilter} size={20}/>
-            </TouchableOpacity>
-   
-         
-
-
-      {/* <SearchBar
-        platform='ios'
-        placeholder='Where to?'
-        value={searchInput} 
-        onBlur={undefined} 
-        onChangeText={handleInputChange} 
-        onFocus={undefined} 
-        clearIcon={false}
-        searchIcon={{ name: 'search' }} 
-        loadingProps={undefined} 
-        showLoading={false} 
-        onClear={undefined} 
-        onCancel={undefined} 
-        lightTheme={false} 
-        round={true} 
-        cancelButtonTitle={''} 
-        cancelButtonProps={undefined} 
-        showCancel={false}
-        // style={{backgroundColor:'red'}}     
-        />
-        */}
+            <View style={styles.searchBarView}>
+                <TouchableOpacity 
+                    style={styles.searchBar}
+                    activeOpacity={0.8}
+                >
+                    <FontAwesomeIcon icon={faSearch} style={styles.searchIcon} size={20}/>
+                    <Text style={styles.placeholderHeading}>Where To?</Text>
+                    <Text style={styles.placeholderSubheading}>Anywhere · Any week · Add guests</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.filterIconView}>
+                <TouchableOpacity 
+                    style={styles.filterIconContainer}
+                    activeOpacity={0.9}
+                >
+                    <FontAwesomeIcon icon={faFilter} size={20}/>
+                </TouchableOpacity>
+            </View>
         </View> 
-    </SafeAreaView>
   );
 }
 
@@ -62,16 +40,19 @@ const styles = StyleSheet.create({
         borderBottomWidth: 1
         // alignItems: 'center'
     },
+    searchBarView: {
+        width: 320,
+        marginLeft: 25,
+        borderRadius: 30,
+    },
     searchBar: {
         backgroundColor: 'white',
         height: 50,
         width: 320,
         borderColor: '#e4e4e4',
         borderWidth: 0.4,
-        shadowColor: '#e4e4e4',
         borderRadius: 30,
         justifyContent:'center',
-        marginLeft: 25
     },
     placeholderHeading: {
         marginLeft: 60,
@@ -89,15 +70,21 @@ const styles = StyleSheet.create({
         position: 'absolute',
         marginLeft: 20
     },
+    filterIconView: {
+        width: 40,
+        height: 40,
+        position: 'absolute',
+        left: 360,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 30,
+    },
     filterIconContainer: {
         width: 40,
         height: 40,
         borderColor: '#b8b8b8',
         borderWidth: 1.5,
         borderRadius: 30,
-        position: 'absolute',
-        left: 360,
-        bottom: 5,
         justifyContent: 'center',
         alignItems: 'center'
     },
