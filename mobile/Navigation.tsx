b81a2e355773d5fd3a27d77dafb3fed6a2bdd71a
Krystal <kabkimmel@gmail.com>
2024-02-09 20:39:04 -0600

    modified navigator to use createNativeStackNavigator

--- mobile/Navigation.tsx
+++ mobile/Navigation.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { createStackNavigator } from '@react-navigation/stack';
+// import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import Home from "./screens/Home";
 import SpotDetail from "./screens/SpotDetail";
 import Reviews from "./screens/Reviews";
 
 const RootNavigation:React.FC = (): JSX.Element => {
-    const Stack = createStackNavigator();
+    const Stack = createNativeStackNavigator();
 
     const screenOptions = {
         headerShown: false,
@@ -15,13 +16,20 @@ const RootNavigation:React.FC = (): JSX.Element => {
     return (
         <NavigationContainer>
             <Stack.Navigator screenOptions={screenOptions} initialRouteName="Home">
-                <Stack.Screen options={{animationEnabled: false}} name='Home' component={Home} />
+                <Stack.Screen name='Home' component={Home} />
+                <Stack.Screen name='SpotDetail' component={SpotDetail} />
+                <Stack.Screen name='Reviews' component={Reviews} />
+                <Stack.Screen name='Wishlists' component={Reviews} />
+                <Stack.Screen name='Trips' component={Reviews} />
+                <Stack.Screen name='Inbox' component={Reviews} />
+                <Stack.Screen name='Profile' component={Reviews} />
+                {/* <Stack.Screen options={{animationEnabled: false}} name='Home' component={Home} />
                 <Stack.Screen name='SpotDetail' component={SpotDetail} />
                 <Stack.Screen name='Reviews' component={Reviews} />
                 <Stack.Screen options={{animationEnabled: false}} name='Wishlists' component={Reviews} />
                 <Stack.Screen options={{animationEnabled: false}} name='Trips' component={Reviews} />
                 <Stack.Screen options={{animationEnabled: false}} name='Inbox' component={Reviews} />
-                <Stack.Screen options={{animationEnabled: false}} name='Profile' component={Reviews} />
+                <Stack.Screen options={{animationEnabled: false}} name='Profile' component={Reviews} /> */}
             </Stack.Navigator>
         </NavigationContainer>
     )
