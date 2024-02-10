d661927f505e45de15dd5ba2c3db939ae51495ab
Krystal <kabkimmel@gmail.com>
2024-02-09 19:36:47 -0600

    Made adjustments to the placement of the "Guest Favorite" text and heart icon

--- mobile/screens/Home/Components/Spots.tsx
+++ mobile/screens/Home/Components/Spots.tsx
@@ -1,5 +1,5 @@
 import React, {memo, useEffect, useState} from 'react';
-import { View, Pressable, StyleSheet, ActivityIndicator} from 'react-native';
+import { View, Pressable, StyleSheet, ActivityIndicator, Text} from 'react-native';
 import SpotDetails from '../../Home/Components/SpotDetails'
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
@@ -14,9 +14,9 @@ interface IHome {
     navigation: any
 };
 
-// interface IHeartPressed {
-//     [key:number]: boolean
-// }
+interface IHeartPressed {
+    [key:number]: boolean
+};
 
 
 const Spots:React.FC<IHome> = ({navigation}) => {
@@ -24,7 +24,7 @@ const Spots:React.FC<IHome> = ({navigation}) => {
     const [currPage, setCurrPage] = useState(1);
     const [paginatedData, setPaginatedData] = useState<Spot[]>([]);
     const size = 10;
-    // const [isHeartPressed, setIsHeartPressed] = useState<IHeartPressed>({});
+    const [isHeartPressed, setIsHeartPressed] = useState<IHeartPressed>({});
 
     useEffect(() => {
         const getInfo = async () => {
@@ -45,17 +45,39 @@ const Spots:React.FC<IHome> = ({navigation}) => {
         })
     };
 
+       const toggleHeartPress = (spotId:number) => {
+        setIsHeartPressed(prev => ({
+            ...prev,
+            [spotId]: !prev[spotId]
+        }));
+    };
+
     const renderSpots = ({ item }: { item: Spot }) => {
         return (
           <View style={styles.spotImageView}>
             <Pressable style={styles.spotImageContainer} onPress={() => goToSpotDetail(item)}>
+            <View style={styles.heartContainer}>
+                <View>
+                    {item.id % 2 === 0 && (
+                        <View style={styles.guestFavContainer}>
+                            <Text style={styles.guestFavText}>Guest favorite</Text>
+                        </View>
+                    )}
+                </View>
+                <Pressable style={styles.heartIcon} onPress={() => toggleHeartPress(item.id)}>
+                <FontAwesomeIcon
+                    icon={faHeart}
+                    size={20}
+                    color={isHeartPressed[item.id] ? '#FF385B' :'#535350'}
+                    />
+                </Pressable>
+            </View>
               <Image style={styles.image} source={{ uri: item.previewImage }} />
               <SpotDetails spot={item} />
             </Pressable>
           </View>
         );
     };
-
 
     const renderLoading = () => {
         return (
@@ -64,14 +86,6 @@ const Spots:React.FC<IHome> = ({navigation}) => {
         </View>
         );
     };
-
-
-   // const toggleHeartPress = (spotId:number) => {
-    //     setIsHeartPressed(prev => ({
-    //         ...prev,
-    //         [spotId]: !prev[spotId]
-    //     }));
-    // };
 
 
     return (     
@@ -84,50 +98,8 @@ const Spots:React.FC<IHome> = ({navigation}) => {
             ListFooterComponent={isLoaded ? null : renderLoading}
         />
     );
-}
+};
 
-
-
-
-
-//     return (
-//     <ScrollView>
-//         {allSpots?.map((spot, idx) => (
-//             <View
-//                 key={`spot-${idx}-${date}-${sec}`}
-//                 style={styles.spotImageView}
-//             >
-//                 {idx % 2 === 0 && (
-//                     <View style={styles.guestFavContainer}>
-//                         <Text style={styles.guestFavText}>Guest favorite</Text>
-//                     </View>
-//                 )}
-//                 <Pressable
-//                     style={styles.heartIcon}
-//                     onPress={() => toggleHeartPress(spot.id)}
-//                 >
-            
-//                     <FontAwesomeIcon
-//                         icon={faHeart}
-//                         size={25}
-//                         color={isHeartPressed[spot.id] ? '#FF385B' :'#535350'}
-//                     />
-//                 </Pressable>
-//                 <Pressable
-//                     style={styles.spotImageContainer}
-//                     onPress={()=> goToSpotDetail(spot)}
-//                 >
-//                     <Image
-//                         style={styles.image}
-//                         source={{ uri: spot.previewImage}}
-//                     />
-//                     <SpotDetails spot={spot}/>
-//                 </Pressable>
-//             </View>
-//         ))}
-//     </ScrollView>
-//     );
-// };
 
 const styles = StyleSheet.create({
     spotImageView: {
@@ -149,27 +121,31 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         backgroundColor:'#FFFFFF',
     },
-    // heartIcon: {
-    //     // position: 'absolute',
-    //     // top: 10,
-    //     // right: 45,
-    //     zIndex: 1
-    // },
-    // guestFavContainer: {
-    //     backgroundColor: "#FBFBFB",
-    //     borderColor: '#A19F9D',
-    //     borderRadius: 20,
-    //     // position: 'absolute',
-    //     // right: 220,
-    //     justifyContent: 'center',
-    //     alignItems: 'center',
-    //     width: 125,
-    //     height: 25,
-    //     zIndex: 1
-    // },
-    // guestFavText: {
-    //     ...fonts.subHeader
-    // },
+    heartContainer: {
+        position: 'absolute',
+        top: 15,
+        zIndex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: 355,
+    },
+    heartIcon: {
+        marginHorizontal: 15
+    },
+    guestFavContainer: {
+        backgroundColor: "#FBFBFB",
+        borderColor: '#A19F9D',
+        borderRadius: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 125,
+        height: 25,
+        marginHorizontal: 15
+    },
+    guestFavText: {
+        ...fonts.subHeader
+    },
     loading: {
         flexDirection: 'row',
         justifyContent: 'center',
