457a78a98a1fdb45a5b9addd4b42246ec6a4e948
Krystal <kabkimmel@gmail.com>
2024-02-19 11:14:27 -0600

    added pressable to exit modal

--- mobile/screens/Home/Components/FilterModal.tsx
+++ mobile/screens/Home/Components/FilterModal.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Modal, Text } from 'react-native';
+import { View, Modal, Pressable, Text, StyleSheet } from 'react-native';
 
 interface IFilterModalProps {
     isVisible:boolean,
@@ -10,15 +10,39 @@ const FilterModal:React.FC<IFilterModalProps> = ({ isVisible, setIsVisible }) =>
   return (
     <View>
         <Modal
-            animationType='fade'
+            animationType='slide'
             transparent={false}
             visible={isVisible}
-            presentationStyle='overFullScreen'
+            presentationStyle='pageSheet'
         >
+        <View>
+            <Pressable style={styles.closeModal} onPress={() => setIsVisible(false)}>
+                <Text style={{fontWeight: "600"}}>x</Text>
+            </Pressable>
+        </View>
 
         </Modal>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+    closeModal: {
+        marginTop: 60,
+        marginLeft: 15,
+        width: 25,
+        height: 25,
+        position: 'absolute',
+        elevation: 5,
+        borderColor: '#b9b9b9',
+        borderWidth: 1,
+        borderRadius: 30,
+        backgroundColor: '#FFFFFF',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+})
+
+
 
 export default FilterModal;
